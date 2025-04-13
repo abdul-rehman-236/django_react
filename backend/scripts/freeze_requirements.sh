6ebc6159e355d60632f3d3236fdfd926fd2c3a53
Abdul <abdul.rehman.3091149236@gmail.com>
2025-04-13 03:18:28 -0700

    Check virtual environment is activated while generating requirements.txt file

--- backend/scripts/freeze_requirements.sh
+++ backend/scripts/freeze_requirements.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+echo "Check if the virtual environment is activated"
+
+if [[ -z "$VIRTUAL_ENV" ]]; then
+  echo "Virtual environment not activated. Activating..."
+
+  source backend/env/bin/activate
+fi
+
 echo "Checking for new packages..."
+
 pip freeze > backend/requirements.txt
+
+echo "Updating requirements.txt"
+
 git add backend/requirements.txt
+
 echo "Updated requirements.txt"
